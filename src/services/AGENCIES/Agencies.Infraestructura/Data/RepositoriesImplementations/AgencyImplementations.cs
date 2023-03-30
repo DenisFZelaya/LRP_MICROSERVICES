7d59414e0ec9d4eb333e1797b44f9bd2d06700cd
Denis Federico Zelaya <Denisfzelaya@gmail.com>
2023-03-29 21:19:02 -0600

    Agregando arquitectura N-CAPAS

---
+++ src/services/AGENCIES/Agencies.Infraestructura/Data/RepositoriesImplementations/AgencyImplementations.cs
@@ -0,0 +1,53 @@
+﻿
+using Agencies.Infraestructura.Context;
+using Agencies.Infraestructura.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Agencies.Infraestructura.Data.RepositoriesImplementations
+{
+    public class AgencyImplementations
+    {
+        private readonly AGENCIESContext _dbContext;
+
+        public AgencyImplementations(AGENCIESContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Agencias>> GetAllAsync()
+        {
+            return await _dbContext.Agencias.ToListAsync();
+        }
+
+        public async Task<Agencias> GetByIdAsync(int id)
+        {
+            return await _dbContext.Agencias.FindAsync(id);
+        }
+
+        public async Task<Agencias> AddAsync(Agencias agency)
+        {
+            await _dbContext.Agencias.AddAsync(agency);
+            await _dbContext.SaveChangesAsync();
+            return agency;
+        }
+
+        public async Task UpdateAsync(Agencias agency)
+        {
+            _dbContext.Entry(agency).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var agency = await GetByIdAsync(id);
+            _dbContext.Agencias.Remove(agency);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/services/AGENCIES/Agencies.Infraestructura/Interfaces/IAgencyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Agencies.Infraestructura.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agencies.Infraestructura.Interfaces
+{
+    public interface IAgencyRepository
+    {
+        Task<IEnumerable<Agencias>> GetAllAsync();
+        Task<Agencias> GetByIdAsync(int id);
+        Task<Agencias> AddAsync(Agencias agency);
+        Task UpdateAsync(Agencias agency);
+        Task DeleteAsync(int id);
+    }
+}
