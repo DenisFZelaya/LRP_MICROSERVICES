e471862462696646bd7b867bd9343f674818a0dc
Denis Federico Zelaya <Denisfzelaya@gmail.com>
2023-04-01 11:58:33 -0600

    MS-AGENCIES-Funcional

---
+++ src/services/AGENCIES/Agencies.API/Controllers/AgenciasController.cs
@@ -0,0 +1,23 @@
+﻿using Agencies.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agencies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgenciasController : ControllerBase
+    {
+        readonly AGENCIESContext _context;
+        public AgenciasController(AGENCIESContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return _context.Agencias.ToList();
+        }
+    }
+}
--- src/services/AGENCIES/Agencies.API/Program.cs
+++ src/services/AGENCIES/Agencies.API/Program.cs
@@ -1,4 +1,10 @@
+﻿using Agencies.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 
@@ -6,6 +12,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddEntityFrameworkSqlServer()
+.AddDbContext<AGENCIESContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AgenciesContext"));
+});
+
+
 
 var app = builder.Build();
 
